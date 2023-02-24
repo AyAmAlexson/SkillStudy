58ed4b015f2930fc3d2b23a76476d7ee66bddee7
AyAmAlexson <a.molodenko@gmail.com>
2023-02-25 00:29:55 +0100

    Update main.py

--- TicTacToeMod2PY/main.py
+++ TicTacToeMod2PY/main.py
@@ -1,26 +1,111 @@
 
 
 def init_field(n=3):   #Инициализируем чистое игровое поле
+    ch=["0", "A", "B", "C"]
     field = [["-" for j in range(n+1)] for i in range(n+1)]
     for i in range (n+1):
-        field[0][i], field[i][0] = i, i
+        field[0][i], field[i][0] = ch[i], i
     field[0][0] = " "
     return field
 
 def print_field(field): #Распечатываем игровое поле
     print('\n'.join('\t'.join(map(str, row)) for row in field))  # Вывести матрицу красиво
 
-def gameplay():
+def make_move_player(field,char):
+    cols = ["a","b","c"]
+    rows = ["1","2","3"]
+    while True:
+        move = input ("Введите поле для вашего хода в формате A1: ")
+        if move[0].lower() in cols and move[1] in rows:
+            if field[int(move[1])][cols.index(move[0].lower()) + 1] == "-":
+                field[int(move[1])][cols.index(move[0].lower())+1]= char
+                print_field(field)
+                return field
+            else:
+                print ("Это поле занято, повторите ввод")
+
+        else:
+            print ("Неверный ввод поля, повторите ввод")
+
+
+def make_move_cpu(field,cpu_char,player_char):
+
+    
+
+    for i in range (1,4):
+        for j in range (1,4):
+            if field[i][j] == "-":
+                field[i][j] = char
+                print_field(field)
+                return field
+
+    return field
+
+def check_win(field):
+    for i in range (1,4):
+        if ((field[i][1]==field[i][2]==field[i][3]) and field[i][1]!="-") or ((field[1][i]==field[2][i]==field[3][i] and field[1][i]!="-")):
+
+            return True
+        if ((field[1][1]==field[2][2]==field[3][3]) or (field[3][1]==field[2][2]==field[1][3])) and field[2][2]!="-":
+            return True
+    return False
+
+def check_full(field):
+    for i in range (1,4):
+        for j in range (1,4):
+            if field[i][j] == "-":
+                return False
+    return True
+
+def gameplay(field,player_turn):
+
+    if player_turn:
+        player_char,cpu_char = "X","O"
+    else:
+        cpu_char,player_char = "X","O"
+
+    while True:
+        if player_turn:
+            field = make_move_player(field, player_char)
+            if check_win(field):
+                return 1
+            elif check_full(field):
+                return 0
+            else:
+                player_turn = not player_turn
+        else:
+            field = make_move_cpu(field, cpu_char,player_char)
+            if check_win(field):
+                return 2
+            elif check_full(field):
+                return 0
+            else:
+                player_turn = not player_turn
 
 
 
 
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('PyCharm')
 
     game_field = init_field()
     print_field(game_field)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+    start_choice = input ("Желаете начать первым? 1: Да; 2: Нет  ")
+
+    if start_choice=="1":
+        pl_turn = True
+    elif start_choice=="2":
+        pl_turn = False
+
+
+    winner = gameplay (game_field,pl_turn)
+    if winner == 1:
+        print ("Победа!")
+    elif winner == 2:
+        print ("Поражение!")
+    else:
+        print ("Ничья!")
