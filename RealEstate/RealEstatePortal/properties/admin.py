4e67aec8dff69ce9cf5c85a5c98b703ea9e66b7e
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2023-07-17 01:04:02 +0200

    Update admin.py

--- RealEstate/RealEstatePortal/properties/admin.py
+++ RealEstate/RealEstatePortal/properties/admin.py
@@ -1,3 +1,17 @@
+
+from .models import *
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Agents)
+admin.site.register(ResProperties)
+admin.site.register(Clients)
+admin.site.register(Owners)
+admin.site.register(CustomUser)
+admin.site.register(Comments)
+admin.site.register(Contracts)
+admin.site.register(Viewings)
+admin.site.register(Features)
+admin.site.register(PropertiesFeatures)
+
+
